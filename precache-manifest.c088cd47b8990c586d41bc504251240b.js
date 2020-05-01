a56a4df699059a41baa34825f3b1cfe9bb8efc67
Devesh Srivastava <devesh.s@gtgt.in>
2020-05-01 12:54:43 +0530

    Updates

---
+++ precache-manifest.c088cd47b8990c586d41bc504251240b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd65d02974dd4db97615e7d04e71ec90",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "05e7133fd5c596d2f321",
+    "url": "/monsters-rolodex/static/css/main.7773f390.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05e7133fd5c596d2f321",
+    "url": "/monsters-rolodex/static/js/main.68694083.chunk.js"
+  },
+  {
+    "revision": "0c81cb278443448182f0",
+    "url": "/monsters-rolodex/static/js/runtime-main.165419b0.js"
+  }
+]);
